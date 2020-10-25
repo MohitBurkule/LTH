18b49cb75b4b4067538e171f82e0f72b6722f9e9
mohit <bitbucketfecommon@mylocus.in>
2020-10-25 19:19:49 +0530

    added daily data trends

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
-st.beta_set_page_config(layout="wide")
+#st.beta_set_page_config(layout="wide")
 
 st.title("Overview of historical weather in Pune")
 st.text("dataset source - kaggle ")
@@ -14,6 +14,13 @@ def load_data(nrows):
 	data = pd.read_csv('pune.csv', nrows=nrows,infer_datetime_format = True)
 	data['date_time'] = pd.to_datetime(data['date_time'], format='%Y-%m-%d %H:%M:%S')
 	#data.set_index('date_time')
+	return data
+
+@st.cache
+def load_day_data(nrows):
+	data = pd.read_csv('pune_daily.csv', nrows=nrows,infer_datetime_format = True)
+	data['date_time'] = pd.to_datetime(data['date_time'], format='%d-%m-%Y')
+	data.set_index('date_time')
 	return data
 
 datelimits = st.date_input("dates range ",[datetime.date(2019, 7, 6), datetime.date(2019, 7, 15)],min_value=datetime.date(2009, 1, 1),max_value=datetime.date(2020, 1, 1))
@@ -28,11 +35,18 @@ except:
 steps=st.number_input('x axis steps', value=10,min_value=1)
 
 st.write(startindex,endindex)
-df = pd.DataFrame(data[startindex:endindex ], columns = ['tempC'])
+
+cols1=st.multiselect(label='select data to visualize ', options=list(data.columns),default=['tempC',])
+df = pd.DataFrame(data[startindex:endindex ], columns = cols1)
 fig=plt.figure()
 plt.plot(df,figure=fig)
 plt.xticks(rotation=90,ticks=[i  for i in range(startindex,endindex,steps)],labels=[data['date_time'][i].date()  for i in range(startindex,endindex,steps)],figure=fig)
 st.pyplot(fig)
 
+st.subheader("daily temperature ")
+daily_data=load_day_data(None).set_index('date_time')
+cols=st.multiselect(label='select data to visualize ', options=list(daily_data.columns),default=['avg_tempC',])
+daily_data=pd.DataFrame(daily_data,columns=cols)
+st.line_chart(daily_data)
 #st.altair_chart(df)
 #st.line_chart(df)
